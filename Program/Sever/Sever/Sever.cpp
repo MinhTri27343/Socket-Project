359dc29370e814fbf41891443680bd199e9ef399
TriNguyen1208 <168810923+TriNguyen1208@users.noreply.github.com>
2024-07-09 16:49:26 +0700

    Update Sever.cpp DTri complete code

--- Program/Sever/Sever/Sever.cpp
+++ Program/Sever/Sever/Sever.cpp
@@ -33,141 +33,54 @@ int main()
         else
         {
             // =============================================================
-
-
-
-
             AfxSocketInit(NULL);
             CSocket sever;
             CSocket client;
             sever.Create(1234);
-            cout << "Server is listenning!\n";
+            cout << "Server is listening!\n";
             sever.Listen();
             if (sever.Accept(client))
             {
-                cout << "Connect is succesfull!\n";
+                cout << "Connect is succesful!\n";
 
                 //===========================================Sent file list_file=============================================================
-                string list_file = "list_file.txt";
-                cout << "Sending list file!\n";
-                unsigned long long size_list_file = readSizeFile(list_file);
-                client.Send(&size_list_file, sizeof(size_list_file), 0);
-                ifstream in;
-                in.open(list_file,ios::binary);  
-                if(!in.is_open())
-                {
-
-                    cout << "Send list file falled!\n";
-                }
-                else
-                {
-                    char* buffer = new char[size_list_file];
-                    in.read(buffer, size_list_file);
-                    client.Send(buffer, size_list_file, 0);
-                    delete[] buffer;
-                    cout << "Sending file text is successful!\n";
-                    in.close();
-                }
+                SendInfoAllFileToClient(ref(client));
                 //===========================================Sent file list_file=============================================================
 
                 //============================================Receive list file need download=====================================================
-                 
                 while (true)
                 {
-                    unsigned long long len = 0;
-                    client.Receive(&len, sizeof(len), 0);//Receives length of string
-                    char* buffer_receive = new char[len + 1];
-                    client.Receive(buffer_receive, len, 0);//Receives string
-                    buffer_receive[len] = '\0';
-                    
-                    string file_recei = "list_file_recei.txt";
-
-                    writeFileRecei(file_recei, buffer_receive);
-
-                    delete[] buffer_receive;
-               
+                    ifstream in;
+                    ReadFileNeedDownFromClient(ref(client));
+                    cout << "Received file need to download!\n";
                     //===========Send file ==============================================================
-
+                    string file_recei = "list_file_recei.txt";
                     in.open(file_recei);
-                  
+
                     if (!in.is_open())
                     {
                         cout << "Read file error";
+                        return 0;
                     }
                     else
                     {
-                       
                         string temp_2;
-                        while (!in.eof())
+                        while (getline(in, temp_2, '\n'))
                         {
-                          
-                            getline(in, temp_2, '\n');
-                            if (temp_2 == "")
-                            {
-            
-                                continue;
-                            }
-                            //=======================Send file temp_2======================================
-                            cout << "Sending file " << temp_2 << " for client!\n";
-                            unsigned long long size_file = readSizeFile(temp_2);
-                            unsigned long long size_tmp2 = temp_2.length();
-                            client.Send(&size_tmp2, sizeof(size_tmp2), 0);//Send length of name file
-                            client.Send(&temp_2, size_tmp2, 0);//Send name file
-                            client.Send(&size_file, sizeof(size_file), 0);//Send size of file
-
-                            ifstream in_send;
-                            in_send.open(temp_2, ios::binary);
-                            int size_buff = 1;
-                            char* buff_send = new char[size_buff];
-                            while (true)
-                            {
-                                in_send.read(buff_send, size_buff);
-                                int size_read = in_send.gcount();
-                                
-                                if (size_read < size_buff)
-                                {
-                                    client.Send(buff_send, size_read, 0);
-                                    break;
-                                }
-                                else
-                                {
-                                    client.Send(buff_send, size_buff, 0);
-
-                                }
-                                if (in_send.eof())break;
-
-
-                            }
-                            delete[]buff_send;
-                            in_send.close();
-                            cout << "Client download file successful!\n";
-                            //=======================Send file temp_2======================================
+                            SendInfo1FileToClient(ref(client), temp_2); //đúng rồi khỏi sửa
+                            Send1FileToClient(ref(client), temp_2);
                         }
                         in.close();
-                       
+                        //===========Send file ============================================================
                     }
-
-               
-                    //===========Send file =============================================================
-
                 }
-
-
-                
                 //============================================Receive list file need download=====================================================
-
-               
-
-
-
             }
             else
             {
                 cout << "Connect is falled!\n";
-                
+
             }
-
-
             // =============================================================
 
         }
