86eb0db4ac4d2bf4112a92e2aadb3c2bcdadbd8e
TriNguyen1208 <168810923+TriNguyen1208@users.noreply.github.com>
2024-07-09 16:44:17 +0700

    Update Func.h DTri Complete code

--- Program/Client/Func.h
+++ Program/Client/Func.h
@@ -8,3 +8,8 @@ char* fileDownload(char* Check, string file_check1, string file_check2, unsigned
 void ShowCur(bool CursorVisibility);
 void createBox(unsigned long long x, unsigned long long y, unsigned long long width, unsigned long long height);
 void gotoxy(unsigned long long x, unsigned long long y);
+void SendFileNeedDownToServer(CSocket& client, string file_user, unsigned long long& size_pre_file, unsigned long long& size_after_file, string file_check1, string file_check2, bool& checkIsExist, char*& send);
+void ReceiveInfo1FileFromServer(CSocket& client, unsigned long long& size_name_file_download, unsigned long long& size_file_download, char*& name_file_download);
+void ReceiveInfoAllFileFromServer(CSocket& client);
+void Receive1FileFromServer(CSocket& client, char* name_file_download, unsigned long long size_file_download);
+int NumOfFile(char* c);
