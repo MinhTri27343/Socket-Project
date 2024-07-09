55f4a872b58a792381601b4ef96483202fb37ae2
TriNguyen1208 <168810923+TriNguyen1208@users.noreply.github.com>
2024-07-09 16:47:16 +0700

    Update Func.h DTri complete code

--- Program/Sever/Sever/Func.h
+++ Program/Sever/Sever/Func.h
@@ -3,3 +3,7 @@
 using namespace std;
 void writeFileRecei(string file_recei, char* buffer_receive);
 unsigned long long readSizeFile(string file_name);
+void ReadFileNeedDownFromClient(CSocket& client);
+void SendInfo1FileToClient(CSocket& client, string temp_2);
+void Send1FileToClient(CSocket& client, string fileNeedDown);
+void SendInfoAllFileToClient(CSocket& client);
