392ed9e9ed7380890696099f56fc81140f1e6646
TriNguyen1208 <168810923+TriNguyen1208@users.noreply.github.com>
2024-07-10 16:55:36 +0700

    Update Func.h DTri done n clients and ctrl C

--- Program/Sever/Sever/Func.h
+++ Program/Sever/Sever/Func.h
@@ -3,7 +3,7 @@
 using namespace std;
 void writeFileRecei(string file_recei, char* buffer_receive);
 unsigned long long readSizeFile(string file_name);
-void ReadFileNeedDownFromClient(CSocket& client);
-void SendInfo1FileToClient(CSocket& client, string temp_2);
-void Send1FileToClient(CSocket& client, string fileNeedDown);
-void SendInfoAllFileToClient(CSocket& client);
+bool ReadFileNeedDownFromClient(CSocket& client);
+bool SendInfo1FileToClient(CSocket& client, string temp_2);
+bool Send1FileToClient(CSocket& client, string fileNeedDown);
+bool SendInfoAllFileToClient(CSocket& client);
