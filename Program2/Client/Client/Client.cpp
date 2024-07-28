0967be935fe79633f4606a5b5f2ce68d66f9a592
TriNguyen1208 <168810923+TriNguyen1208@users.noreply.github.com>
2024-07-28 21:38:56 +0700

    Update Client.cpp DTri sua

--- Program2/Client/Client/Client.cpp
+++ Program2/Client/Client/Client.cpp
@@ -4,7 +4,10 @@
 #include "pch.h"
 #include "framework.h"
 #include "Client.h"
-
+#include<conio.h>
+#include<functional>
+#include"Func.h"
+//#pragma pack(push, 1)
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
@@ -34,6 +37,46 @@ int main()
         else
         {
             // TODO: code your application's behavior here.
+            AfxSocketInit(NULL);
+            CSocket client;
+            char ip[100];
+            int port;
+            client.Create();
+            cout << "\t \t \t \t \t  ____ _     ___ _____ _   _ _____ \n";
+            cout << "\t \t \t \t \t / ___| |   |_ _| ____| \\ | |_   _|\n";
+            cout << "\t \t \t \t \t| |   | |    | ||  _| |  \\| | | |  \n";
+            cout << "\t \t \t \t \t| |___| |___ | || |___| |\\  | | |  \n";
+            cout << "\t \t \t \t \t \\____|_____|___|_____|_| \\_| |_|  \n";
+            // input
+            /*cout << "Input IP address of server: "; cin >> ip;
+            cout << "Input port of server: "; cin >> port;*/
+
+            if (client.Connect(CA2W("127.0.0.1"), 1234))
+            {
+                cout << "\nClient has connected to the server";
+                cout << endl;
+                vector<pair<ofstream, File>> v;
+                vector<File> files;
+                
+                ReceiveInfoAllFileFromServer(ref(client));
+                vector<string> list_file = readListFile();
+                signal(SIGINT, SignalCallBack);
+                //Thread check 2s
+                thread check_2s(checkInput, ref(files), list_file);
+                check_2s.detach();
+
+                while (true)
+                {
+                    ReceiveFileDownloadToClient(ref(client), v, files);
+                }
+                
+            }
+            else
+            {
+                cout << "\nUnable to connect to the server";
+            }
+            client.Close();
+            
         }
     }
     else
