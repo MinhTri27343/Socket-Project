bd0f2d61d2711216e7650fc594f0372040cab291
TriNguyen1208 <168810923+TriNguyen1208@users.noreply.github.com>
2024-07-28 21:40:49 +0700

    Create Func.h

---
+++ Program2/Server/Server/Func.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "pch.h"
+#include "framework.h"
+
+#define NORMAL 1
+#define HIGH 4
+#define CRITICAL 10
+#define size_buff 20480
+using namespace std;
+
+struct File
+{
+    int size_file_name = 0;
+    char* file_name = NULL;
+    unsigned long long size_file = 0;
+    unsigned long long current_size_file = 0;
+    int priority = 0;
+};
+unsigned long long readSizeFile(string file_name);
+bool SendInfoAllFileToClient(CSocket& client);
+void Send1Chunk(CSocket& client, vector<pair<ifstream, File>>& v, int index);
+void isCheckUpdate(CSocket& client, vector<pair<ifstream, File>>& v);
+void SendFileDownloadToClient(CSocket& client, vector<pair<ifstream, File>>& v);
