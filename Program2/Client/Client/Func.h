5e94e0375c86dd0748a35283838e57f44237dafc
Minh Luan <luanthuco11@gmail.com>
2024-08-01 23:05:03 +0700

    Luan: display

--- Program2/Client/Client/Func.h
+++ Program2/Client/Client/Func.h
@@ -16,10 +16,15 @@ struct File
     int priority = 0;
 };
 void ReceiveInfoAllFileFromServer(CSocket& client);
-void gotoxy(unsigned long long x, unsigned long long y);
 void Receive1Chunk(CSocket& client, vector<pair<ofstream, File>>& v, int index);
 void checkIsUpdate(CSocket& client, vector<pair<ofstream, File>>& v, vector<File>& tmp);
-void ReceiveFileDownloadToClient(CSocket& client, vector<pair<ofstream, File>>& v, vector<File>& tmp);
+void ReceiveFileDownloadToClient(CSocket& client, vector<pair<ofstream, File>>& v, vector<File>& tmp, int width_max);
 void checkInput(vector<File>& files, vector<string>list_file);
 vector<string> readListFile();
 void SignalCallBack(int signum);
+//======================Display============================
+void gotoxy(int x, int y);
+void createBox(int  x, int  y, int  width, int  height);
+void deleteContent(int x, int y, int width, int height);
+void ShowCur(bool CursorVisibility);
+void displayPercent(int& number_of_file, vector<File> files, int& width_max);
