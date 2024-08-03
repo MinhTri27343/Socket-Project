1c9fabb84e973f7fcfbeb3ee4651dfbc888b7f9a
TriNguyen1208 <168810923+TriNguyen1208@users.noreply.github.com>
2024-08-03 15:06:04 +0700

    Update Func.cpp DTri fix

--- Program2/Client/Client/Func.cpp
+++ Program2/Client/Client/Func.cpp
@@ -129,7 +129,6 @@ void Receive1Chunk(CSocket& client, vector<pair<ofstream, File>>& v, int index)
         client.Send((char*)&total_byte_sum, sizeof(total_byte_sum), 0);
         while (total_byte_sum < size_buff)
         {
-            cout << endl << "Hello";
             byte = client.Receive(buff_receive, size_buff - total_byte_sum, 0);
             v[index].first.write(buff_receive, byte);
             total_byte_sum += byte;
@@ -169,6 +168,8 @@ void checkIsUpdate(CSocket& client, vector<pair<ofstream, File>>& v, vector<File
         size_pre = size_cur;
     }
     client.Send(&num_of_file, sizeof(num_of_file), 0);
+    bool isSuccessful;
+    client.Receive(&isSuccessful, sizeof(bool), 0);
     if (num_of_file > 0)
     {
         int v_size = size_pre;
@@ -200,10 +201,10 @@ void checkIsUpdate(CSocket& client, vector<pair<ofstream, File>>& v, vector<File
 }
 void ReceiveFileDownloadToClient(CSocket& client, vector<pair<ofstream, File>>& v, vector<File>& tmp, int width_max)
 {
-    {
+    //{
         checkIsUpdate(ref(client), v, tmp);
-        std::lock_guard<std::mutex> guard(mtx);
-    }
+        //std::lock_guard<std::mutex> guard(mtx);
+    //}
     for (int i = 0; i < tmp.size(); i++) //edit
     {
         if (tmp[i].current_size_file == tmp[i].size_file)
@@ -212,12 +213,16 @@ void ReceiveFileDownloadToClient(CSocket& client, vector<pair<ofstream, File>>& 
             cout << "100%";
         }
     }
+    bool isDone = true;
+    client.Send((char*)&isDone, sizeof(isDone), 0);
     for (int i = 0; i < v.size(); i++)
     {
         for (int j = 0; j < v[i].second.priority; j++)
         {
             //Todo: receive 1 chunk 1024 byte to client
             Receive1Chunk(client, v, i);
+            bool isDone = true;
+            client.Send((char*)&isDone, sizeof(isDone), 0);
             // ============= Display DownLoad ========================
 
             for (int k = 0; k < tmp.size(); k++) //edit
@@ -344,10 +349,10 @@ void checkInput(vector<File>& files, vector<string>list_file)
             else if (temp_3 == "HIGH") file_new.priority = 4;
             else if (temp_3 == "CRITICAL") file_new.priority = 10;
             file_need.push_back(file_new.file_name);
-            {
-                std::lock_guard<std::mutex> guard(mtx);
+            //{
+                //std::lock_guard<std::mutex> guard(mtx);
                 files.push_back(file_new);
-            }
+            //}
         }
         fin.close();
         this_thread::sleep_for(std::chrono::seconds(2));
